e8d4ce566f143b3bdf125681069ef963a86f9aa3
seoyeonyang <ysy8837@gmail.com>
2022-11-03 11:41:32 +0900

    exit 구현

--- src/com/ysy/exam/board/Main.java
+++ src/com/ysy/exam/board/Main.java
@@ -1,7 +1,27 @@
 package com.ysy.exam.board;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("~~");
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("== 게시판 v 0.1 =");
+        System.out.println("== 프로그램 시작 =");
+
+        while (true) {
+            System.out.printf("명령)");
+            String cmd = sc.nextLine();
+
+            if (cmd.equals("exit")) {
+                break;
+            }
+            System.out.println("입력된 명령어 : " +cmd);
+
+        }
+            System.out.println("== 프로그램 종료 ==");
+            sc.close();
+
     }
 }
