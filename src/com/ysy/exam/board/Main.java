e201b5445de3727f08d0dd78aabd00fb40ac5ed3
seoyeonyang <ysy8837@gmail.com>
2022-11-03 13:07:19 +0900

    새 게시물 입력 받기 기능 구현 시작

--- src/com/ysy/exam/board/Main.java
+++ src/com/ysy/exam/board/Main.java
@@ -15,13 +15,28 @@ public class Main {
             String cmd = sc.nextLine();
 
             if (cmd.equals("exit")) {
+                System.out.println("==프로그램 종료==");
                 break;
-            }
-            System.out.println("입력된 명령어 : " +cmd);
+            } else if (cmd.equals("/user/article/write")) {
+                System.out.println("== 게시물 등록 ==");
 
+                System.out.printf("제목: ");
+                String title = sc.nextLine();
+
+                System.out.printf("내용: ");
+                String content = sc.nextLine();
+
+                int id = 1;
+                System.out.println(id + "번 게시물이 등록되었습니다.");
+            }
+
+           else {
+                System.out.println("입력된 명령어 : " + cmd);
+            }
         }
-            System.out.println("== 프로그램 종료 ==");
-            sc.close();
+
+
+        sc.close();
 
     }
 }
